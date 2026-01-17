41282a24c5ff1e2a5fdb10c783497780c9eb882a
IvanYahodkin <yagodkinivan1808@gmail.com>
2026-01-17 18:52:39 +0200

    add models

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class DishType(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    cooks = models.ManyToManyField(Cook)
